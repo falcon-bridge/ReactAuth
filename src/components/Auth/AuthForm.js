ea2523a89f2dd10a3a61abbab0f9c375554955d2
Aayush <falcon.bridge2000@gmail.com>
2022-02-05 01:37:36 +0530

    added functionality to manage error handling in user sign up

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -20,6 +21,7 @@ const AuthForm = () => {
 
     // optional validation
 
+    setIsLoading(true);
     if (isLogin) {
       // ...
     } else {
@@ -37,12 +39,20 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false);
         if (res.ok) {
           // ...
         } else {
           return res.json().then((data) => {
             //show an error modal
             console.log(data);
+            console.log(data.error);
+            console.log(data.error.message);
+            let errorMessage = "Authentication failed !";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
           });
         }
       });
@@ -67,7 +77,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
