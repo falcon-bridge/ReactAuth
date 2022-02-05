7aecfcd192f549556009982b45255d69d13c1812
Aayush <falcon.bridge2000@gmail.com>
2022-02-05 23:37:56 +0530

    added auto-logout feature

--- src/store/auth_contect.js
+++ src/store/auth_contect.js
@@ -7,20 +7,32 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjustedExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuaration = adjustedExpirationTime - currentTime;
+  return remainingDuaration;
+};
+
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem("token");
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggedin = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token", token);
-  };
-
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+
+    setTimeout(logoutHandler, remainingTime);
   };
 
   const contextValue = {
